79dba855a20ca1aaa9b607814099dbe2ddefd745
Julia Kim <juliaykim6@gmail.com>
2021-02-15 16:59:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
-require_relative "./user.rb"
+#require_relative "./user.rb"
+class Student 
+  attr_accessor :first_name, :last_name
 
-class Student < User
-
+  def initialize(first_name, last_name)
+    @first_name = first_name 
+    @last_name = last_name
+  end
+  
 
 end
